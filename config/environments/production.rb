61fdfb25102688e41fe97bad139f2c8d986e7844
minhphanhvu <minhphanh.vu@gmail.com>
2021-04-09 21:07:13 -0500

    Prep for launch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://tranquil-headland-18313.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://tranquil-headland-18313.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
